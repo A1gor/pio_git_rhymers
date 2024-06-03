97d53a0a62abe62dfe774eb6027fd2f94c7daac6
A1gor <fordzio222@gmail.com>
2024-06-03 11:08:33 +0200

    17. Dodanie dokumentacji do klasy HanoiRhymer i jej metod

--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -1,12 +1,29 @@
 package edu.kis.vh.nursery;
 
+/**
+ * The HanoiRhymer class extends the defaultCountingOutRhymer class.
+ * It adds functionality to count the number of rejected values based on
+ * a specific condition: if the value to be added is greater than the current top value in the stack.
+ */
 public class HanoiRhymer extends defaultCountingOutRhymer {
 
+    /**
+     * The total number of rejected values.
+     */
     int totalRejected = 0;
 
+    /**
+     * Returns the total number of rejected values.
+     */
     public int reportRejected() {
         return totalRejected;
     }
+
+    /**
+     * Adds a value to the stack. If the value is greater than the current top value
+     * and the stack is not empty, the value is rejected and the rejected count is incremented.
+     * Otherwise, the value is added to the stack.
+     */
 
     public void countIn(int in) {
         if (!callCheck() && in > peekaboo())
